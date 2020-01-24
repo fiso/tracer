6392f3c19360663bffe14705d55aa18eed457afe
Filip Haglund <filip.l.haglund@gmail.com>
2020-01-24 10:44:08 +0100

    Use png instead of tga

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ const {Sphere} = require('./primitives/sphere');
 const {Vector} = require('./vector');
 const {Worker} = require('worker_threads');
 const {performance} = require('perf_hooks');
+const PNG = require('pngjs').PNG;
 
 function constructScene () {
   const scene = new Scene();
@@ -108,33 +109,16 @@ function reportStatus (status) {
   process.stdout.write(`\r${status}\r`);
 }
 
-function writeTga (pixels, filename, w, h) {
-  const buffer = Buffer.alloc(18 + pixels.length * 4);
-  buffer.writeInt8(0, 0);
-  buffer.writeInt8(0, 1);
-  buffer.writeInt8(2, 2);
-  buffer.writeInt16LE(0, 3);
-  buffer.writeInt16LE(0, 5);
-  buffer.writeInt8(0, 7);
-  buffer.writeInt16LE(0, 8);
-  buffer.writeInt16LE(0, 10);
-  buffer.writeInt16LE(w, 12);
-  buffer.writeInt16LE(h, 14);
-  buffer.writeInt8(32, 16);
-  buffer.writeInt8(0, 17);
+function writePng (pixels, filename, width, height) {
+  const png = new PNG({
+      width,
+      height,
+      filterType: -1,
+  });
 
-  let offset = 18;
-  for (let i = 0; i < h; i++) {
-      for (let j = 0; j < w; j++) {
-          const idx = ((h - i - 1) * w + j) * 4;
-          // BGRA order
-          buffer.writeUInt8(pixels[idx + 2], offset++);
-          buffer.writeUInt8(pixels[idx + 1], offset++);
-          buffer.writeUInt8(pixels[idx], offset++);
-          buffer.writeUInt8(pixels[idx + 3], offset++);
-      }
-  }
-  fs.writeFileSync(filename, buffer);
+  png.data = pixels;
+
+  png.pack().pipe(fs.createWriteStream(filename));
 }
 
 async function execute () {
@@ -142,9 +126,9 @@ async function execute () {
   try {
     const scene = constructScene();
     const camera = new Camera();
-    for (let frame = 0; frame < 24; frame++) {
+    for (let frame = 0; frame < 1; frame++) {
       const pixels = await render(scene, camera);
-      writeTga(pixels, `frame_${frame}.tga`, w, h);
+      writePng(pixels, `frame_${frame}.png`, w, h);
     }
   } catch (e) {
     console.log(e);
